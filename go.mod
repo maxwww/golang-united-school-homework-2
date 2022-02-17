e70738e4f66491703b7763208ee758f89788edf7
Maksym Soroka <ms.mikro.1@gmail.com>
2022-02-17 16:40:12 +0200

    implement CalcSquare func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maxwww/golang-united-school-homework-2
+
+go 1.15
--- solution.go
+++ solution.go
@@ -1,12 +1,26 @@
 package square
 
-// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
+import (
+	"math"
+)
 
-// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
-// it's like:
-// CalcSquare(10.0, SidesTriangle)
-// CalcSquare(10.0, SidesSquare)
-// CalcSquare(10.0, SidesCircle)
+type Side int
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+const (
+	SidesTriangle Side = 3
+	SidesSquare   Side = 4
+	SidesCircle   Side = 0
+)
+
+func CalcSquare(sideLen float64, sidesNum Side) float64 {
+	switch sidesNum {
+	case SidesSquare:
+		return math.Pow(sideLen, 2)
+	case SidesTriangle:
+		return math.Pow(sideLen, 2) * math.Sqrt(3) / 4
+	case SidesCircle:
+		return math.Pi * math.Pow(sideLen, 2)
+	}
+
+	return 0
 }
